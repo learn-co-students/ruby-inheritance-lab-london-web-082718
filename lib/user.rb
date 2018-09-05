2af32e3cd3029ecde2f48469416b3cb0a2e77fa0
braj011 <bhagyaraj01@gmail.com>
2018-09-05 16:39:39 +0100

    Done.

---
+++ lib/user.rb
@@ -0,0 +1,14 @@
+
+
+ # Parent Class which both Student and Teacher Classes will inherit from
+
+ class User
+
+   attr_accessor :first_name, :last_name
+
+# Don't need to initialize anything!!!
+
+# TASK:  Define the User class such that a user can have a first and last name.
+# You'll need both a setter and a getter for first and last name.
+
+ end
